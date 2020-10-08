8c8247dd844d37a64c7ab364ffa64240d31cdf6d
Boris Rozanov <boris.rozanov@heig-vd.ch>
2020-10-08 16:08:54 +0200

    [Matlab]: transition problem (no solution) with test script: TestAllNGC_BR.m (not all g-codes) see figures with end- and lift-off points... (only Helix) - center coord. precision from interpreter ?? (new helix definition) - other unmasked problem ??

---
+++ common/Validate_OpenCN/Validate_OpenCN_BR.m
@@ -0,0 +1,107 @@
+% This file shows an example how validation of OpenCN could be realized (RHG, oct 2, 2020)
+clc; clear; close all;
+
+%% Config
+global DebugConfig
+DebugConfig = 0;
+
+cfg = FeedoptDefaultConfig;
+cfg.NDiscr = 20;
+cfg.NBreak = 10;
+cfg.NHorz = 5;
+
+EnableDebugLog(DebugCfg.Transitions);
+
+%% Choose directory with G-code validation files
+fs         = filesep; % file separation character according to Win/Linux
+Gdir       = uigetdir('.', 'Choose directory with G-code validation files');
+dircontent = dir([Gdir, fs, '*.ngc']);
+% dircontent = dircontent(3:end);  % suppress . and ..
+Str        = sprintf('%d G-code files found', length(dircontent));
+uiwait(msgbox(Str,'','modal'));
+
+%% Choose parameter file
+[Pfile, Ppath] = uigetfile('*.m');
+PfileName      = [Ppath, Pfile];
+run(PfileName);
+Str        = sprintf('%d different parameter settings', length(param));
+uiwait(msgbox(Str,'','modal'));
+
+%% Initialization
+Ok         = 1;
+ErrorStr   = {};
+MEcell     = {};
+ProfilCell = {};
+ErrorCtr   = 0;
+NCtr       = 0;
+NGcodes    = length(dircontent);
+Nparam     = length(param);
+Ntot       = NGcodes*Nparam;
+
+%% Log file
+diary ([cfg.LogFileName, '_', ...
+            datestr(now,'yyyy_mm_dd_HH_MM_SS'), ...
+            '.txt']);
+diary on;
+
+%% setup wait bar
+fw = waitbar(0, '');
+
+%% sweep through all G-Code files and all parameter settings
+for k = 1:NGcodes
+    cfg.source = [dircontent(k).folder, fs, dircontent(k).name];
+    DebugLog(DebugCfg.Transitions, [cfg.source, '\n']);
+    ctx = InitFeedoptPlan(cfg);
+%     for l = 1:Nparam
+    for l = 1:1
+%         pause(0.2);  % remove later
+        NCtr = NCtr + 1;
+%         Str  = sprintf('processing G-code file %s, parameter setting %d', dircontent(k).name(1:3), l);
+%         waitbar(NCtr/Ntot, fw, Str); % update waitbar 
+        try
+            profile on
+            ctx = FeedoptPlanRun(ctx);
+            ProfilCell{k, l} = profile('info');
+        catch ME
+            profile off
+            Ok       = 0;
+            ErrorCtr = ErrorCtr + 1;
+            Str      = sprintf('%s did not work with parameter setting %d', dircontent(k).name, l);
+            ErrorStr{ErrorCtr} = Str;
+%             msgbox(ErrorStr,'','modal');
+            MEcell{ErrorCtr}   = ME;
+        end
+    end
+end
+%
+close(fw);
+diary off;
+%
+d         = date;
+Cd        = cd;
+GdirSplit = regexp(Gdir, fs, 'split');
+GdirEnd   = GdirSplit{end};
+
+%% make directory and copy all relevant files and directories
+if Ok == 1
+    DirName = [Cd, fs, 'Val_', GdirEnd, '_', Pfile(1:end-2), '_', 'OK'];
+else
+    DirName = [Cd, fs, 'Val_', GdirEnd, '_', Pfile(1:end-2), '_', 'FAILED'];
+end
+%
+if exist(DirName, 'dir') == 7
+   rmdir(DirName, 's');   % sometimes this line throws an error
+end
+
+mkdir(DirName);
+SubGDir = [DirName, fs, GdirEnd];
+mkdir(SubGDir);
+copyfile(PfileName, DirName);                          % copy selected parameter file
+copyfile('Validate_OpenCN.m', DirName);                % copy this .m source file
+copyfile(Gdir, SubGDir);                               % copy all G-code files
+save([DirName, '_status'], ...
+     'ErrorStr', 'd', 'MEcell', 'ProfilCell');         % save relevant data to status.mat file
+
+
+
+
--- common/c_planner_v5/CalcTransition.m
+++ common/c_planner_v5/CalcTransition.m
@@ -5,10 +5,8 @@ CutOff=ctx.cfg.CutOff;
 CollTolDeg=ctx.cfg.CollTolDeg;
 Length_Threshold=3*CutOff;
 
-DebugLog(DebugCfg.Transitions, ...
-    '========== CalcTransition ==========\n')
-DebugLog(DebugCfg.Transitions, ...
-    'CutOff = %.3f\n', CutOff)
+% DebugLog(DebugCfg.Transitions, ...
+%     '========== CalcTransition ==========\n')
 
 if IsEnabledDebugLog(DebugCfg.Global)
     PrintCurvStruct(ctx, CurvStruct1);
@@ -17,12 +15,14 @@ end
 
 CurvStruct_T = CurvStruct1; %default value
 
-[~, r0D1_1] = EvalCurvStruct(ctx, CurvStruct1, 1);
-[~, r1D1_1] = EvalCurvStruct(ctx, CurvStruct2, 0);
+[r0D0_1, r0D1_1] = EvalCurvStruct(ctx, CurvStruct1, 0);
+[r0D0_2, r0D1_2] = EvalCurvStruct(ctx, CurvStruct1, 1);
+[r1D0_1, r1D1_1] = EvalCurvStruct(ctx, CurvStruct2, 0);
+[r1D0_2, r1D1_2] = EvalCurvStruct(ctx, CurvStruct2, 1);
 
 % colinearity test
 if CurvStruct1.Type~=CurveType.Helix && CurvStruct2.Type~=CurveType.Helix && ...
-        collinear(r0D1_1, r1D1_1, CollTolDeg) % && norm(r0D2 - r1D2) < 10*eps && collinear(r0D2, r1D2, 1e-2)
+        collinear(r0D1_2, r1D1_1, CollTolDeg) % && norm(r0D2 - r1D2) < 10*eps && collinear(r0D2, r1D2, 1e-2)
     
     status = TransitionResult.Collinear;    
     CurvStruct1_C = CurvStruct1;
@@ -97,6 +97,9 @@ else
     
 end
 
+% DebugLog(DebugCfg.Transitions, ...
+%     'CutOff = %.3f\n', CutOff)
+
 status = TransitionResult.Ok;
 
 CurvStruct1_C = CutCurvStruct(ctx, CurvStruct1, 0, CutOff);
@@ -114,16 +117,16 @@ end
 [r1D0, r1D1, r1D2] = EvalCurvStruct(ctx, CurvStruct2_C, 0);
 
 % G2 transition calculation
-[p5, status, alpha0, alpha1] = G2_Hermite_Interpolation(r0D0, r0D1, r0D2, r1D0, r1D1, r1D2);
+[p5, ret, alpha0, alpha1] = G2_Hermite_Interpolation(r0D0, r0D1, r0D2, r1D0, r1D1, r1D2);
 
-if status==1
+if ret==1
     
     % standard case
     % transition CurvStruct calculation
     CurvStruct_T = ConstrTransP5Struct(p5, CurvStruct1.FeedRate);
     status = TransitionResult.Ok;
 
-elseif status==6
+elseif ret==6
     
     % TODO: decide in the future...
     % Now we ignore and construct the transition curve anyway
@@ -138,19 +141,19 @@ elseif status==6
         'Lines: %d, %d\n\n', CurvStruct1.gcode_source_line, ...
         CurvStruct2.gcode_source_line);
     
-    if coder.target('matlab')
-
-        figure;
-        PlotCurvStructsBR(ctx, [CurvStruct1 CurvStruct_T CurvStruct2]);
-        hold on;
-        plot3(r0D0(1), r0D0(2), r0D0(3), 'xr', 'LineWidth', 3);
-        hold on;
-        plot3(r1D0(1), r1D0(2), r1D0(3), 'xr', 'LineWidth', 3);
-        title({ctx.cfg.source, 'status_G2_Hermite=6'}, 'Interpreter', 'none');
-        axis equal;
-        camproj('perspective');
-
-    end
+%     if coder.target('matlab')
+% 
+%         figure;
+%         PlotCurvStructsBR(ctx, [CurvStruct1 CurvStruct_T CurvStruct2]);
+%         hold on;
+%         plot3(r0D0(1), r0D0(2), r0D0(3), 'xr', 'LineWidth', 3);
+%         hold on;
+%         plot3(r1D0(1), r1D0(2), r1D0(3), 'xr', 'LineWidth', 3);
+%         title({ctx.cfg.source, 'status_G2_Hermite=6'}, 'Interpreter', 'none');
+%         axis equal;
+%         camproj('perspective');
+% 
+%     end
        
 else
     
@@ -172,7 +175,16 @@ else
         plot3(r0D0(1), r0D0(2), r0D0(3), 'xr', 'LineWidth', 3);
         hold on;
         plot3(r1D0(1), r1D0(2), r1D0(3), 'xr', 'LineWidth', 3);
+        hold on;
+        plot3(r0D0_1(1), r0D0_1(2), r0D0_1(3), 'xc', 'LineWidth', 3);
+        hold on;
+        plot3(r0D0_2(1), r0D0_2(2), r0D0_2(3), 'xc', 'LineWidth', 3);
+        hold on;
+        plot3(r1D0_1(1), r1D0_1(2), r1D0_1(3), 'xc', 'LineWidth', 3);
+        hold on;
+        plot3(r1D0_2(1), r1D0_2(2), r1D0_2(3), 'xc', 'LineWidth', 3);
         title({ctx.cfg.source, 'No solution'}, 'Interpreter', 'none');
+        axis equal;
         camproj('perspective');
 
     end
