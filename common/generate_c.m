9c1dcc8bbf8b0c862f366f53f9fec8b5756670fc
rozanov <boris.rozanov@heig-vd.ch>
2022-02-04 15:32:31 +0100

    generation script improved

--- common/generate_c.m
+++ common/generate_c.m
@@ -1,34 +1,36 @@
 clear; clc;
 
-generate_for_arm = false;
+generate_for_arm_32 = true;
+generate_for_x86_64 = false;
+generate_for_arm_64 = false;
 
 cfg = coder.config('lib', 'ecoder', true);
 cfg.IndentSize = 4;
 cfg.BuildConfiguration = 'Faster Runs';
 cfg.ColumnLimit = 100;
 cfg.GenCodeOnly = true;
-% cfg.FilePartitionMethod = 'SingleFile';
-cfg.InlineBetweenUserFunctions = 'Readability';
+cfg.InlineBetweenUserFunctions = 'Speed';
 cfg.PreserveArrayDimensions = true;
 cfg.GenerateReport = false;
 cfg.GenerateCodeMetricsReport = false;
 cfg.SupportNonFinite = false;
 cfg.TargetLang = 'C++';
 cfg.TargetLangStandard = 'C++11 (ISO)';
-cfg.MATLABSourceComments = false;
+cfg.MATLABSourceComments = true;
 cfg.EnableAutoExtrinsicCalls = false;
 
-% cfg.Hardware = 'Intel->x86-64 (Linux 64)';
-if ~generate_for_arm
+if  generate_for_x86_64
     cfg.HardwareImplementation.TargetHWDeviceType = 'Intel->x86-64 (Linux 64)';
     cfg.HardwareImplementation.ProdHWDeviceType = 'Intel->x86-64 (Linux 64)';
     cfg.CodeReplacementLibrary = 'Intel SSE (Linux)';
-%     cfg.CodeReplacementLibrary = 'None';
     output_root = '../x86/matlab/generated';
-else
+elseif generate_for_arm_32
+    cfg.HardwareImplementation.TargetHWDeviceType = 'ARM Compatible->ARM Cortex';
+    cfg.HardwareImplementation.ProdHWDeviceType = 'ARM Compatible->ARM Cortex';
+    output_root = '../arm/matlab/generated';
+elseif generate_for_arm_64
     cfg.HardwareImplementation.TargetHWDeviceType = 'ARM Compatible->ARM 64-bit (LP64)';
     cfg.HardwareImplementation.ProdHWDeviceType = 'ARM Compatible->ARM 64-bit (LP64)';
-%     cfg.CodeReplacementLibrary = 'rpi4';
     output_root = '../arm64/matlab/generated';
 end
 
@@ -42,7 +44,6 @@ cfg.SILPILCheckConstantInputs = false;
 cfg.SILPILSyncGlobalData = false;
 cfg.SaturateOnIntegerOverflow = false;
 cfg.PreserveVariableNames = 'All';
-% cfg.StackUsageMax = 8096*1024;
 
 cfg.HighlightPotentialDataTypeIssues = true;
 cfg.HighlightPotentialRowMajorIssues = true;
@@ -52,48 +53,29 @@ cfg.CustomHeaderCode = '#include "functions.h"';
 configType = coder.OutputType('FeedoptDefaultConfig');
 ctxType = coder.OutputType('InitFeedoptPlan');
 resampleType = coder.OutputType('ResampleState');
+SplineType = coder.OutputType('bspline_create');
+CurvType = coder.OutputType('ConstrCurvStructType');
 
-% C = ConstrLineStruct([0,0,0]', [0,0,0]', 0.1, ZSpdMode.NN);
-			C = coder.OutputType('ConstrCurvStructType');
+global sqrt_calls sin_calls cos_calls cot_calls DebugConfig
+sqrt_calls = 0;
+sin_calls = 0;
+cos_calls = 0;
+cot_calls = 0;
+DebugConfig = 0;
 
-	splineType = coder.OutputType('bspline_create');
-
-	global sqrt_calls sin_calls cos_calls cot_calls DebugConfig
-	sqrt_calls = 0;
-	sin_calls = 0;
-	cos_calls = 0;
-	cot_calls = 0;
-	DebugConfig = 0;
-
-	% 
-	% R0D0 = [0,0,0]';
-	% codegen('-config', cfg, '-d', 'gen_c/g2hermite',...
-	%     'G2_Hermite_Interpolation', '-args', {R0D0,R0D0,R0D0,R0D0,R0D0,R0D0});
-	% 
-	% 
-	% return;
-	% %% FEEDOPT USERSPACE
-	codegen('-config', cfg,'-d', output_root, ...
-		'sinspace', '-args', {2,3,int32(20)},...
-		'EvalCurvStruct', '-args', {ctxType, C, coder.typeof(0.0, [1, Inf], [0, 1])},...
-		'c_roots_', '-args', coder.typeof(0, [1, 12], [0, 1]),...
-		'PrintCurvStruct', '-args', {ctxType, C},...
-		'CalcTransition', '-args', {ctxType, C, C},...
-		'FeedoptDefaultConfig',...
-		'InitFeedoptPlan', '-args', configType, ...
-		'FeedoptPlan', '-args', ctxType,...
-		'linspace', '-args', {0,1,100},...
-		'ConstrCurvStructType', ...
+codegen('-config', cfg,'-d', output_root, '-o', 'opencn_matlab',...
+    'EvalCurvStruct', '-args', {ctxType, CurvType, coder.typeof(0.0, [1, Inf], [0, 1])},...
+	'FeedoptDefaultConfig',...
+	'InitFeedoptPlan', '-args', configType,...
+	'FeedoptPlan', '-args', ctxType,...
+	'linspace', '-args', {0,1,100},...
+    'CalcTransition', '-args', {ctxType, CurvType, CurvType},...
+	'ConstrCurvStructType', ...
     'ConstrLineStruct', '-args', {[0,0,0]', [0,0,0]', 0.1, ZSpdMode.NN},...
-    'ConstrHelixStruct', '-args', {[0,0,0]', [0,0,0]', [0,0,0]', 0.0, [0,0,0]', 0, 0, 0, ZSpdMode.NN},...
     'ConstrHelixStructFromArcFeed', '-args', {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,[0,0,0]'},...
     'bspline_create', '-args', {int32(4), linspace(0,1,10)},...
     'ResampleState', '-args', 0,...
-    'ResampleNoCtx', '-args', {resampleType, splineType, C},...
-    'EvalPosition', '-args', {C, C, 0},...
-    'bspline_copy', '-args', splineType,...
-    'CopyCurvStruct', '-args', C,...
-    'EnableDebugLog', '-args', DebugCfg.Warning,...
-    'DisableDebugLog', '-args', DebugCfg.Warning,...
+    'ResampleNoCtx', '-args', {resampleType, SplineType, CurvType},...
+    'EvalPosition', '-args', {CurvType, CurvType, 0},...
+    'bspline_copy', '-args', SplineType,...
     'ConfigSetSource', '-args', {configType, coder.typeof(' ', [1, 1024], [0, 1])});
-
