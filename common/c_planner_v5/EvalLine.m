4c000efa4cf694763ad598ed0dec1b82826c1083
quentin <quentin.perrot@heig-vd.ch>
2022-04-26 09:46:41 +0200

    EvalLine modification for 5 axis

--- common/c_planner_v5/EvalLine.m
+++ common/c_planner_v5/EvalLine.m
@@ -1,4 +1,16 @@
 function [r0D, r1D, r2D, r3D] = EvalLine(CurvStruct, u_vec)
+% EvalLineND :
+%
+% Input 
+% CurvStruct    : struct : Line segment
+% u_vec         :    1xn : Vector for evaluation of the curv
+% 
+% Output
+% r0D           :   nDxn : Evaluation of the line at u\_vec points
+% r1D           :   nDxn : 1rst order parametric derivative for the line at u\_vec points
+% r2D           :   nDxn : 2nd order parametric derivative for the line at u\_vec points
+% r3D           :   nDxn : 3rd order parametric derivative for the line at u\_vec points
+
 if ~coder.target('MATLAB')
 coder.cinclude('common/tracy/Tracy.hpp');
 coder.inline('never')
@@ -8,16 +20,16 @@ end
 % parametrization of a straight line between P0 and P1
 P0  = CurvStruct.P0;
 P1  = CurvStruct.P1;
+% Calculate the numbre of dimension 
+[nD, ~] = size(P0);
 %
-r0D = [P1(1)*u_vec + P0(1)*(1-u_vec);
-       P1(2)*u_vec + P0(2)*(1-u_vec);
-       P1(3)*u_vec + P0(3)*(1-u_vec)];
+r0D = P1.*u_vec + P0.*(1-u_vec);
 %
 N   = length(u_vec);
 r1D = repmat(P1 - P0, 1, N);
 %
-r2D = zeros(3, N);
-r3D = zeros(3, N);
+r2D = zeros(nD, N);
+r3D = zeros(nD, N);
 
    
     
